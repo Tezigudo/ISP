3ad1c68f5aea91c2efc70337065f2e36014a59e9
Tezigudo <80502090+Tezigudo@users.noreply.github.com>
2022-12-30 09:53:56 +0700

    delete overflow file

--- git-automate-Tezigudo/app/finish.py
+++
@@ -1,2 +0,0 @@
-def finish():
-    print("Shutting down the app")
--- git-automate-Tezigudo/main.py
+++
@@ -1,8 +0,0 @@
-# this won't work until you use git to move these files
-# to the correct directory
-from app.start import start
-from app.finish import finish
-
-if __name__ == '__main__':
-    start()
-    finish()
